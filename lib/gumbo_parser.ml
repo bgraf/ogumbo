d8f74862a5b293937e8c2fcad770a78a74e0ba0f
bgraf <bgraf@uos.de>
2015-11-30 20:00:20 +0100

    + source position record type

--- lib/gumbo_parser.ml
+++ lib/gumbo_parser.ml
@@ -1,1 +1,8 @@
+
+type source_position = {
+    line      : int;
+    column    : int;
+    offset    : int;
+  }
+
 external parse : string -> Gumbo_output.t = "ogumbo_parse"
--- lib/gumbo_parser.mli
+++ lib/gumbo_parser.mli
@@ -1,1 +1,8 @@
+
+type source_position = {
+    line      : int;
+    column    : int;
+    offset    : int;
+  }
+
 val parse : string -> Gumbo_output.t
